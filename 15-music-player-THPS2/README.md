32f4b9d07f256c1072c6492b8016fa2a0dcdcf72
Rafael Correa <contato.mundustech@gmail.com>
2022-10-16 16:01:47 -0300

    Commit THPS2

---
+++ 15-music-player-THPS2/README.md
@@ -0,0 +1,4 @@
+# 15-music-player-THPS2
+ #15 Projeto criado com os conhecimentos obtidos no curso do Igor Oliveira - Programador Br
+---
+[Music Player THPS2](https://rafaelcorrea00.github.io/15-music-player-THPS2/)
---
+++ 15-music-player-THPS2/script.js
@@ -0,0 +1,151 @@
+
+
+let video = document.getElementById('thps2_movies')
+
+let artist = document.getElementById("artist")
+
+let music = document.getElementById("music")
+
+function song01(){
+    video.play()
+    video.currentTime = 33
+    artist.innerHTML = "Rage Agaisnt The Machine"
+    music.innerHTML = "Guerrila Radio"
+}
+
+function song02(){
+    video.play()
+    video.currentTime = 132
+    artist.innerHTML = "Powerman 5000"
+    music.innerHTML = "When Worlds Collide"
+}
+
+function song03(){
+    video.play()
+    video.currentTime = 179
+    artist.innerHTML = "Anthrax"
+    music.innerHTML = "Bring The Noise [ft. Chuck D]"
+}
+
+function song04(){
+    video.play()
+    video.currentTime = 222
+    artist.innerHTML = "DubPistols"
+    music.innerHTML = "Cyclone"
+}
+
+function song05(){
+    video.play()
+    video.currentTime = 262
+    artist.innerHTML = "Millencolin"
+    music.innerHTML = "No Cigar"
+}
+
+function song06(){
+    video.play()
+    video.currentTime = 302
+    artist.innerHTML = "Born by Allah & Kareem Campbell"
+    music.innerHTML = "City Stars"
+}
+
+function song07(){
+    video.play()
+    video.currentTime = 346
+    artist.innerHTML = "Lagwagon"
+    music.innerHTML = "May 16"
+}
+
+function song08(){
+    video.play()
+    video.currentTime = 388
+    artist.innerHTML = "Black Papper Plane"
+    music.innerHTML = "Out With the Old [ft. valley life]"
+}
+
+function song09(){
+    video.play()
+    video.currentTime = 430
+    artist.innerHTML = "Consumed"
+    music.innerHTML = "Heavy Metal Winner"
+}
+
+function song10(){
+    video.play()
+    video.currentTime = 474
+    artist.innerHTML = "Bad Religion"
+    music.innerHTML = "You"
+}
+
+function song11(){
+    video.play()
+    video.currentTime = 517
+    artist.innerHTML = "The High & Mighty"
+    music.innerHTML = "B-Boy Document 99 [ft. Mos Def & Mad Skillz]"
+    music.style.fontSize = "20px";
+}
+
+function song12(){
+    video.play()
+    video.currentTime = 559
+    artist.innerHTML = "Fu Manchu"
+    music.innerHTML = "Evil Eye"
+}
+
+function song13(){
+    video.play()
+    video.currentTime = 601
+    artist.innerHTML = "Papa Roach"
+    music.innerHTML = "Blood Brothers"
+}
+
+function song14(){
+    video.play()
+    video.currentTime = 642
+    artist.innerHTML = "Styles of Beyond"
+    music.innerHTML = "Subculture"
+}
+
+function song15(){
+    video.play()
+    video.currentTime = 677
+    artist.innerHTML = "Swingin' Utters"
+    music.innerHTML = "Five Lessons Learned"
+}
+
+function extra01(){
+    video.play()
+    video.currentTime = 718
+    artist.innerHTML = "Extras"
+    music.innerHTML = "Custom Skater"
+}
+
+function extra02(){
+    video.play()
+    video.currentTime = 761
+    artist.innerHTML = "Extras"
+    music.innerHTML = "Officer Dick"
+}
+
+function extra03(){
+    video.play()
+    video.currentTime = 819
+    artist.innerHTML = "Extras"
+    music.innerHTML = "Private Camera"
+}
+
+function extra04(){
+    video.play()
+    video.currentTime = 895
+    artist.innerHTML = "Extras"
+    music.innerHTML = "Spider-Man"
+}
+
+function extra05(){
+    video.play()
+    video.currentTime = 978
+    artist.innerHTML = "Extras"
+    music.innerHTML = "80's Tony"
+}
+
+// by Rafael Correa | v.1.0
+// Fazer Media Queries
